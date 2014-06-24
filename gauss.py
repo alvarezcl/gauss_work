77cc43044451bdbae4736a2ab68171bed9c4e879
Luis <luis@luis-VirtualBox.(none)>
2014-06-24 16:00:04 -0700

    Comments

--- gauss.py
+++ gauss.py
@@ -11,12 +11,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import pylab as pl
 #from scipy.stats import multivariate_normal
 
+# Function returns a scalar evaluated at x with gaussian function.
 def gauss_1d(x,mean,variance):
     return (1/np.sqrt(2*np.pi*variance))*np.exp(-(x-mean)**2/(2*variance))
 
+# Return the sum of two gaussians that vary a long differing dimensions.
 def sum_gauss(x,mean1,var1,y,mean2,var2):
     return gauss_1d(x,mean1,var2) + gauss_1d(y,mean2,var2)
 
+# Return the two-dimensional gaussian with mean array and cov matrix values.
 def mult_gauss(x,y,mean,cov):
     assert cov[0][1] is cov[1][0]
     assert cov[0][1] <= cov[0][0]*cov[1][1]
@@ -25,7 +28,8 @@ def mult_gauss(x,y,mean,cov):
     pos[:,:,0] = X; pos[:,:,1] = Y
     rv = multivariate_normal(mean,cov)
     return rv, X, Y
-    
+
+# Plot 3d surface and contours given any set of values, X, Y, Z    
 def plot_3d(X,Y,Z):
     fig1 = plt.figure(1)
     ax1 = Axes3D(fig1)
