8c7647c41c8594ed3e61fbffa2346e150c5cdff6
Luis <luis@luis-VirtualBox.(none)>
2014-06-24 21:52:18 -0700

    Added script to produce galaxy contours using MD Gaussian Dist, Updated Function Library

--- multigauss.py
+++ multigauss.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 24 09:36:08 2014
+Created on Tue Jun 24 21:20:27 2014
 
 @author: luis
 """
 
+# Code to test different gaussian distributions
 
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import gauss
-import pylab as pl
+import numpy as np
 
-x = y = np.linspace(-10,10,100)
-mean = [0,0]
-cov = [[1.58**2,1.5],[1.5,1.58**2]]
-Z,X,Y = gauss.mult_gauss(x,y,mean,cov)
+x = y = np.linspace(-20,20,200)
+x0 = 0
+y0 = 0
+varx = 1
+vary = 3
+alpha = np.pi/2
+X,Y,Z = gauss.mult_gaussFun(x,y,x0,y0,varx,vary,alpha)
+
+xp0 = 5
+yp0 = 5
+varxp = 8
+varyp = 2
+alphap = 0
+X,Y,Zp = gauss.mult_gaussFun(x,y,xp0,yp0,varxp,varyp,alphap)
+
+fig1 = gauss.plot_3d(X,Y,Z+Zp)
+fig2 = gauss.plot_contour(Z+Zp)
